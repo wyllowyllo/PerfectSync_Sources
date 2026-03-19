d4ddc1128ae11d4d9af470dea825d04f3a6a3af5
roong-ji <mhienegjyuu@naver.com>
2026-03-19 17:24:33 +0900

    refactor: 겹치는 클래스명 수정

--- Assets/02. Scripts/Test/kmk/PlayerMovementTest.cs
+++ Assets/02. Scripts/Test/kmk/PlayerMovementTest.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody))]
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovementTest : MonoBehaviour
 {
     [Header("Player Settings")]
     [SerializeField] private float _moveSpeed = 5f;
