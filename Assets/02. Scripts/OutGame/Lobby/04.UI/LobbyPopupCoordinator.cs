99636b0ace09fe7c2f8d44fa63a96ba448e05c75
이승빈 <144720218+Pyorang@users.noreply.github.com>
2026-04-01 18:32:29 +0900

    chore : 로비 작업 중

--- Assets/02. Scripts/OutGame/Lobby/04.UI/LobbyPopupCoordinator.cs
+++ Assets/02. Scripts/OutGame/Lobby/04.UI/LobbyPopupCoordinator.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using Photon.Realtime;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -30,6 +28,12 @@ public class LobbyPopupCoordinator : MonoBehaviour
     [SerializeField] private Button _openNicknameChangeButton;
     [SerializeField] private Button _openInviteFriendsButton;
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            HandleEscape();
+    }
+    
     public void ShowPopup(LobbyPopupKind kind)
     {
         if (kind == LobbyPopupKind.EscMenu)
@@ -110,71 +114,6 @@ public class LobbyPopupCoordinator : MonoBehaviour
         _partyInvitePopup?.Hide();
     }
 
-    private static void SetActiveIfExists(GameObject go, bool active)
-    {
-        if (go != null)
-            go.SetActive(active);
-    }
-
-    /*
-    private Coroutine _toastHideRoutine;
-
-    private void Start()
-    {
-        EscMenuPopup.CloseAllPopupsAndEscMenuRequested += HandleEscCloseAllPopupsAndEscMenu;
-        EscMenuPopup.OpenSettingsFromEscMenuRequested += HandleEscOpenSettingsFromMenu;
-        EscMenuPopup.OpenQuitFromEscMenuRequested += HandleEscOpenQuitFromMenu;
-        QuitPopup.CancelRequested += HandleQuitPopupCancelRequested;
-        PartyInvitePopup.OpenPartyInviteFlowRequested += HandleOpenPartyInviteFlow;
-        PartyInvitePopup.PartyInviteClosedOnlyRequested += HandlePartyInviteClosedOnly;
-        PartyInvitePopup.TransientToastRequested += HandlePartyInviteTransientToast;
-
-        if (_openSettingsButton != null)
-            _openSettingsButton.onClick.AddListener(ShowSettings);
-        if (_openNicknameChangeButton != null)
-            _openNicknameChangeButton.onClick.AddListener(ShowNicknameChange);
-        if (_openInviteFriendsButton != null)
-            _openInviteFriendsButton.onClick.AddListener(ShowFollowFriends);
-
-        if (LobbyPartyService.Instance != null)
-        {
-            LobbyPartyService.Instance.OnPartyPartnerLinked += HandlePartyLinkedRefreshVisuals;
-            LobbyPartyService.Instance.OnPartyCleared += HandlePartyClearedRefreshVisuals;
-        }
-
-        RefreshPartyInviteButtonVisuals();
-    }
-
-    private void OnDisable()
-    {
-        EscMenuPopup.CloseAllPopupsAndEscMenuRequested -= HandleEscCloseAllPopupsAndEscMenu;
-        EscMenuPopup.OpenSettingsFromEscMenuRequested -= HandleEscOpenSettingsFromMenu;
-        EscMenuPopup.OpenQuitFromEscMenuRequested -= HandleEscOpenQuitFromMenu;
-        QuitPopup.CancelRequested -= HandleQuitPopupCancelRequested;
-        PartyInvitePopup.OpenPartyInviteFlowRequested -= HandleOpenPartyInviteFlow;
-        PartyInvitePopup.PartyInviteClosedOnlyRequested -= HandlePartyInviteClosedOnly;
-        PartyInvitePopup.TransientToastRequested -= HandlePartyInviteTransientToast;
-
-        if (_openSettingsButton != null)
-            _openSettingsButton.onClick.RemoveListener(ShowSettings);
-        if (_openNicknameChangeButton != null)
-            _openNicknameChangeButton.onClick.RemoveListener(ShowNicknameChange);
-        if (_openInviteFriendsButton != null)
-            _openInviteFriendsButton.onClick.RemoveListener(ShowFollowFriends);
-
-        if (LobbyPartyService.Instance != null)
-        {
-            LobbyPartyService.Instance.OnPartyPartnerLinked -= HandlePartyLinkedRefreshVisuals;
-            LobbyPartyService.Instance.OnPartyCleared -= HandlePartyClearedRefreshVisuals;
-        }
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-            HandleEscape();
-    }
-
     private void HandleEscape()
     {
         if (IsAnyPopupStackShowing())
@@ -185,94 +124,9 @@ public class LobbyPopupCoordinator : MonoBehaviour
 
     private bool IsAnyPopupStackShowing()
     {
-        if (_popupBackgroundRoot != null)
-            return _popupBackgroundRoot.activeSelf;
-        return AnyPopupContentVisible() || (_quitPopup != null && _quitPopup.IsShowing);
+        if (_popupBackgroundRoot != null && _popupBackgroundRoot.activeSelf)
+            return true;
+        else
+            return false;
     }
-
-    private bool AnyPopupContentVisible()
-    {
-        return (_settingsPopup != null && _settingsPopup.IsShowing)
-               || (_nicknameChangePopup != null && _nicknameChangePopup.IsShowing)
-               || (_inviteFriendsPopup != null && _inviteFriendsPopup.IsShowing)
-               || (_partyInvitePopup != null && _partyInvitePopup.IsShowing);
-    }
-
-    private void HandleEscCloseAllPopupsAndEscMenu()
-    {
-        CloseAllPopups();
-    }
-
-    private void HandleEscOpenSettingsFromMenu()
-    {
-        ShowSettings();
-    }
-
-    private void HandleEscOpenQuitFromMenu()
-    {
-        ShowQuitPanel();
-    }
-
-    private void HandleQuitPopupCancelRequested()
-    {
-        CloseAllPopups();
-    }
-
-    private void HandleOpenPartyInviteFlow()
-    {
-        ShowPopup(LobbyPopupKind.PartyInvite);
-    }
-
-    private void HandlePartyInviteClosedOnly()
-    {
-        if (!AnyPopupContentVisible())
-            HidePopupBackground();
-    }
-
-    private void HandlePartyInviteTransientToast(string message)
-    {
-        ShowTransientToast(message);
-    }
-
-    private void HandlePartyLinkedRefreshVisuals(Player _)
-    {
-        RefreshPartyInviteButtonVisuals();
-    }
-
-    private void HandlePartyClearedRefreshVisuals()
-    {
-        RefreshPartyInviteButtonVisuals();
-    }
-
-    private void RefreshPartyInviteButtonVisuals()
-    {
-        bool inParty = LobbyPartyService.Instance != null && LobbyPartyService.Instance.LocalPlayerHasParty;
-        SetActiveIfExists(_openInviteFriendsButtonInviteImage, !inParty);
-        SetActiveIfExists(_openInviteFriendsButtonBackImage, inParty);
-    }
-
-    public void ShowTransientToast(string message)
-    {
-        if (_transientToastText == null)
-            return;
-
-        if (_toastHideRoutine != null)
-        {
-            StopCoroutine(_toastHideRoutine);
-            _toastHideRoutine = null;
-        }
-
-        _transientToastText.text = message ?? string.Empty;
-        _transientToastText.gameObject.SetActive(true);
-        _toastHideRoutine = StartCoroutine(HideTransientToastAfterDelay());
-    }
-
-    private IEnumerator HideTransientToastAfterDelay()
-    {
-        yield return new WaitForSeconds(_transientToastDurationSeconds);
-        if (_transientToastText != null)
-            _transientToastText.gameObject.SetActive(false);
-        _toastHideRoutine = null;
-    }
-    */
 }
