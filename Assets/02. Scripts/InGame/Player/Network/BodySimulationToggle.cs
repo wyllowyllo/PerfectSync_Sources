15bcaadc5049e93e4ab0c448d57c8fe53586d6aa
wyllowyllo <105272901+wyllowyllo@users.noreply.github.com>
2026-04-06 05:21:42 +0900

    chore : remote body 충돌설정

--- Assets/02. Scripts/InGame/Player/Network/BodySimulationToggle.cs
+++ Assets/02. Scripts/InGame/Player/Network/BodySimulationToggle.cs
@@ -10,14 +10,19 @@ namespace InGame.Player.Network
     public class BodySimulationToggle : MonoBehaviour
     {
         [SerializeField] private Rigidbody _rootBody;
+        [SerializeField] private LayerMask _remoteBodyLayer;
 
         private PlayerMovement _playerMovement;
         private PlayerJump _playerJump;
+        private int _originalLayer;
 
         private void Awake()
         {
             _playerMovement = GetComponent<PlayerMovement>();
             _playerJump = GetComponent<PlayerJump>();
+
+            if (_rootBody != null)
+                _originalLayer = _rootBody.gameObject.layer;
         }
 
         private bool _isRemoteBody;
@@ -51,6 +56,7 @@ namespace InGame.Player.Network
             {
                 _rootBody.isKinematic = true;
                 _rootBody.interpolation = RigidbodyInterpolation.Interpolate;
+                _rootBody.gameObject.layer = ToSingleLayer(_remoteBodyLayer);
             }
         }
 
@@ -66,8 +72,19 @@ namespace InGame.Player.Network
             {
                 _rootBody.isKinematic = false;
                 _rootBody.interpolation = RigidbodyInterpolation.Interpolate;
+                _rootBody.gameObject.layer = _originalLayer;
             }
         }
 
+        private static int ToSingleLayer(LayerMask mask)
+        {
+            int value = mask.value;
+            if (value == 0) return 0;
+
+            int layer = 0;
+            while ((value >> layer) > 1)
+                layer++;
+            return layer;
+        }
     }
 }
