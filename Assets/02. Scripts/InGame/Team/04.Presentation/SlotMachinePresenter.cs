aebb138e211070cc1cb4b2beed722c8c85be77d9
wyllowyllo <105272901+wyllowyllo@users.noreply.github.com>
2026-04-21 03:45:03 +0900

    refactor : SlotVfx 생성 로직 분리

--- Assets/02. Scripts/InGame/Team/04.Presentation/SlotMachinePresenter.cs
+++ Assets/02. Scripts/InGame/Team/04.Presentation/SlotMachinePresenter.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using DG.Tweening;
 using InGame.Camera.PlayerCamera;
@@ -88,9 +87,6 @@ namespace InGame.Team
         private int[] _pendingResult;
         private bool _spinStarted;
         private bool _pendingMatch;
-
-        // 릴 정지 완료 시점(=결과 공개 순간)의 슬롯머신 월드 좌표와 매치 여부를 통지.
-        public event Action<Vector3, bool> OnSlotResultRevealed;
 
         private UnityEngine.Camera GetCamera() => _cameraController != null ? _cameraController.OutputCamera : null;
 
@@ -235,8 +231,7 @@ namespace InGame.Team
                 _reactionTween?.Kill();
                 _activeSlotMachine.transform.localScale = _prefabBaseScale;
 
-                // VFX는 구독자(PlayerVfxTrigger)가 처리.
-                OnSlotResultRevealed?.Invoke(_activeSlotMachine.transform.position, _pendingMatch);
+                _activeSlotMachine.PlayResultVfx(_pendingMatch);
 
                 if (_pendingMatch)
                 {
