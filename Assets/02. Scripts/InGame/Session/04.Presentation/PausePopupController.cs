29029e6503c574d1a63ad15448e6e0fba019b0c7
wyllowyllo <105272901+wyllowyllo@users.noreply.github.com>
2026-04-23 15:01:13 +0900

    refactor : 커서 관리 책임 이전

--- Assets/02. Scripts/InGame/Session/04.Presentation/PausePopupController.cs
+++ Assets/02. Scripts/InGame/Session/04.Presentation/PausePopupController.cs
@@ -1,36 +1,23 @@
-using InGame.Camera.PlayerCamera;
-using Unity.Cinemachine;
 using UnityEngine;
 
 public class PausePopupController : MonoBehaviour
 {
     [SerializeField] private GameObject _pausePopup;
 
-    private CinemachineInputAxisController _cameraInput;
-    private bool _cameraInputResolved;
-
     private void Start()
     {
-        ApplyModalState(false);
+        if (_pausePopup != null)
+            _pausePopup.SetActive(false);
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
-        {
             TogglePopup();
-        }
     }
 
-    public void OpenPopup()
-    {
-        SetPopupActive(true);
-    }
-
-    public void ClosePopup()
-    {
-        SetPopupActive(false);
-    }
+    public void OpenPopup() => SetPopupActive(true);
+    public void ClosePopup() => SetPopupActive(false);
 
     public void TogglePopup()
     {
@@ -43,29 +30,6 @@ public class PausePopupController : MonoBehaviour
         if (_pausePopup != null)
             _pausePopup.SetActive(active);
 
-        ApplyModalState(active);
-    }
-
-    private void ApplyModalState(bool modal)
-    {
-        Cursor.lockState = modal ? CursorLockMode.None : CursorLockMode.Locked;
-        Cursor.visible = modal;
-
-        var input = ResolveCameraInput();
-        if (input != null)
-            input.enabled = !modal;
-    }
-
-    private CinemachineInputAxisController ResolveCameraInput()
-    {
-        if (_cameraInputResolved) return _cameraInput;
-
-        var manager = InGameCameraManager.Instance;
-        if (manager == null || manager.FollowCamera == null)
-            return null;
-
-        _cameraInput = manager.FollowCamera.GetComponent<CinemachineInputAxisController>();
-        _cameraInputResolved = true;
-        return _cameraInput;
+        InGameManager.Instance?.SetLocalPaused(active);
     }
 }
